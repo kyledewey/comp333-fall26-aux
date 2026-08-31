080b32a99f47bf88e229bb05dc46302102c864f0
Kyle Dewey <kyle.dewey@csun.edu>
2026-08-31 16:05:49 -0700

    setup for section 2

--- java/section_2/Main.java
+++ java/section_2/Main.java
@@ -3,10 +3,6 @@ import java.io.IOException;
 
 // Motivate ad-hoc polymorphism with a code example
 //
-// Possible destinations:
-// 1.) Terminal
-// 2.) File
-//
 // -Program that takes some command-line input
 //   -Input either empty or it's a file name
 //   -Empty input: write output to terminal
@@ -23,36 +19,13 @@ public class Main {
 	}
     }
 
-    // let's say this runs a long time...
     public static int doCalculation() {
-	int result = 0;
-	for (int x = ...; x < ...; ...x...) {
-	    // runs for awhile
-	    result = x * ...;
-
-	    // problem: lots of output.  Solution:
-	    // output only sometimes.
-	    if (x % 10000) {
-		System.out.println(result);
-	    }
-	}
-	
-	return result;
+	return 1 + 2;
     }
 
     public static void main(String[] args) throws IOException {
 	String filename = getFilename(args);
-
-	// do we really need another method?
-	// int result = 1 + 2; // less code, fewer clock cycles
-	int result = doCalculation(); // more modular
-
-	// int result2 = 1 + 2;
-	// int result2 = doCalculation();
-
-	// Two different ways to write
-	// if/else implies mutual exclusion, but it's
-	// the same sort of thing
+	int result = doCalculation();
 	if (filename == null) {
 	    // write to terminal
 	    System.out.println(result);
