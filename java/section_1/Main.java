8791b0322ab20dbca9c231c2a971311560d5c629
Kyle Dewey <kyle.dewey@csun.edu>
2026-09-02 12:09:03 -0700

    .

--- java/section_1/Main.java
+++ java/section_1/Main.java
@@ -14,6 +14,8 @@ import java.io.IOException;
 // -Program itself does some calculation
 //  (doesn't matter what)
 public class Main {
+    public static FileWriter writer = null;
+
     // returns null if no filename
     public static String getFilename(String[] args) {
 	if (args.length == 0) {
@@ -23,6 +25,24 @@ public class Main {
 	}
     }
 
+    // foo.txt
+    //   abc
+    //   def
+    //
+    // write("abc");
+    // write("def");
+    // close();
+
+    public static void write(int result) {
+	if (writer == null) {
+	    // write to terminal
+	    System.out.println(result);
+	} else {
+	    // write to file
+	    writer.write("" + result + "\n");
+	}
+    }
+    
     // let's say this runs a long time...
     public static int doCalculation() {
 	int result = 0;
@@ -30,22 +50,35 @@ public class Main {
 	    // runs for awhile
 	    result = x * ...;
 
+	    // Clobbering (programming / files)
+	    //
+	    // foo.txt
+	    //  qwertyui
+	    //
+	    // open_for_writing("foo.txt")
+	    //
+	    // foo.txt
+	    //  
+	    
 	    // problem: lots of output.  Solution:
 	    // output only sometimes.
 	    if (x % 10000) {
-		System.out.println(result);
+		write(result);
 	    }
 	}
-	
+
 	return result;
     }
 
     public static void main(String[] args) throws IOException {
 	String filename = getFilename(args);
+	if (filename != null) {
+	    writer = new FileWriter(filename);
+	}
 
 	// do we really need another method?
 	// int result = 1 + 2; // less code, fewer clock cycles
-	int result = doCalculation(); // more modular
+	int result = doCalculation(filename); // more modular
 
 	// int result2 = 1 + 2;
 	// int result2 = doCalculation();
@@ -53,13 +86,9 @@ public class Main {
 	// Two different ways to write
 	// if/else implies mutual exclusion, but it's
 	// the same sort of thing
-	if (filename == null) {
-	    // write to terminal
-	    System.out.println(result);
-	} else {
-	    // write to file
-	    FileWriter writer = new FileWriter(filename);
-	    writer.write("" + result + "\n");
+	write(result);
+
+	if (writer != null) {
 	    writer.close();
 	}
     }
