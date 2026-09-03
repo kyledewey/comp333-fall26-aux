331c4d22be667761f230025073ed8187a7a49b4e
Kyle Dewey <kyle.dewey@csun.edu>
2026-09-02 17:16:16 -0700

    .

--- java/section_2/Main.java
+++ java/section_2/Main.java
@@ -10,6 +10,8 @@ import java.io.IOException;
 // -Program itself does some calculation
 //  (doesn't matter what)
 public class Main {
+    // public static FileWriter writer = null;
+    
     // returns null if no filename
     public static String getFilename(String[] args) {
 	if (args.length == 0) {
@@ -19,34 +21,51 @@ public class Main {
 	}
     }
 
-    public static int doCalculation() {
-	return 1 + 2;
-    }
-
-    // NEXT TIME: doCalculation prints stuff
-    public static void main(String[] args) throws IOException {
-	String filename = getFilename(args);
-
-	// why whole method?
-	int result = 1 + 2; // less lines of code,
-	                    // more clear than doCalculation()
-	                    // efficiency
-	// only using function once! - multiple calls
-	
-	int result = doCalculation(); // modularity
-	                              // cutting code duplication
-	                              // "what it means": intention
-
-	int result2 = 1 + 2;
-	int result2 = doCalculation();
-
-	if (filename == null) {
+    public static void write(FileWriter writer,
+			     int result) {
+	if (writer == null) {
 	    // write to terminal
 	    System.out.println(result);
 	} else {
 	    // write to file
-	    FileWriter writer = new FileWriter(filename);
 	    writer.write("" + result + "\n");
+	}
+    }
+    
+    public static int doCalculation(FileWriter writer) {
+	int result = 0;
+	for (int x = ...; x < ...; x++) {
+	    result += ...x...;
+	    if (x % 10000 == 0) {
+		write(writer, result);
+	    }
+	}
+	return result;
+    }
+
+    // NEXT TIME: address issue of null writer / multiple destinations
+    public static void main(String[] args) throws IOException {
+	String filename = getFilename(args);
+	FileWriter writer = null;
+	if (filename != null) {
+	    writer = new FileWriter(filename);
+	}
+	
+	// why whole method?
+	//int result = 1 + 2; // less lines of code,
+	                    // more clear than doCalculation()
+	                    // efficiency
+	// only using function once! - multiple calls
+	
+	int result = doCalculation(writer); // modularity
+	                              // cutting code duplication
+	                              // "what it means": intention
+
+	// int result2 = 1 + 2;
+	// int result2 = doCalculation();
+	write(writer, result);
+	
+	if (writer != null) {
 	    writer.close();
 	}
     }
